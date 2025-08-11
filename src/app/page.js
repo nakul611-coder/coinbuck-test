d54343b069ad6a071375c7363e7f99279cd74d88
nakul <nakul.ithtech@gmail.com>
2025-08-11 18:55:33 +0530

    test

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <iframe
+      id="dextools-widget"
+      title="DEXTools Trading Chart"
+      width="500"
+      height="500"
+      src="https://www.dextools.io/widget-chart/en/bnb/pe-light/0x28f6f5bb7f79a2f1c43392afc95bff74910dfbaa?theme=dark&chartType=2&chartResolution=30&drawingToolbars=false"
+    ></iframe>
+  );
+}
